326a2a14ec0bff94c4a316ff3e5f10ad4d67a287
isqua <isqua@isqua.ru>
2023-10-12 22:29:00 +0300

    chore: remove extra code

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const appRootElement = document.getElementById('root');
+
+if (!appRootElement) {
+  throw new Error('No app root found');
+}
+
+ReactDOM.createRoot(appRootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
