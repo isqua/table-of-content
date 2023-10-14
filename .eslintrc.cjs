b1f4950638d4b5cd3691a52264a90d9e151cf25b
isqua <isqua@isqua.ru>
2023-10-14 20:43:07 +0300

    style: change indent to 4 spaces

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,33 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/strict-type-checked',
-    'plugin:react-hooks/recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-  ],
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-  },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/strict-type-checked',
+        'plugin:react-hooks/recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
     ],
-  },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: ['./tsconfig.json', './tsconfig.node.json'],
+        tsconfigRootDir: __dirname,
+    },
+    plugins: ['react-refresh'],
+    rules: {
+        'indent': ['error', 4],
+        'semi': ['error', 'never'],
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+        ],
+    },
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-const appRootElement = document.getElementById('root');
+const appRootElement = document.getElementById('root')
 
 if (!appRootElement) {
-  throw new Error('No app root found');
+    throw new Error('No app root found')
 }
 
 ReactDOM.createRoot(appRootElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+)
