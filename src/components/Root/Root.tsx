9a5fcfdc877fa52e8196ea4063aa358485bc0e6e
isqua <isqua@isqua.ru>
2023-10-22 23:36:49 +0300

    chore: add provided data for a demo app

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -1,9 +1,13 @@
 import { Layout } from '../Layout'
+import { Menu } from '../Menu'
+
+import tocData from '../../test/fixtures/toc/idea.json'
 
 export function Root() {
     return (
         <Layout>
             <Layout.Sidebar>
+                <Menu toc={tocData} />
             </Layout.Sidebar>
             <Layout.Main>
                 Hello world
