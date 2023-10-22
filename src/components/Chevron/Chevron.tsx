3c996787501278bd9f6b67536bf46bdc6b633066
isqua <isqua@isqua.ru>
2023-10-22 23:58:56 +0300

    feat: introduce Chevron component

---
+++ src/components/Chevron/Chevron.tsx
@@ -0,0 +1,35 @@
+import clsx from 'clsx'
+import type { MouseEvent } from 'react'
+
+import styles from './Chevron.module.css'
+import ChevronIcon from './Chevron.svg?react'
+
+type ChevronProps = {
+    className?: string
+    open?: boolean
+    onClick?: () => void
+}
+
+export function Chevron({ open, className, onClick }: ChevronProps) {
+    const classNames = clsx(
+        styles.chevron,
+        className,
+        open && styles.open
+    )
+
+    const onClickHandler = (event: MouseEvent) => {
+        event.stopPropagation()
+        event.preventDefault()
+
+        onClick?.()
+    }
+
+    return (
+        <ChevronIcon
+            role="button"
+            aria-expanded={open}
+            onClick={onClickHandler}
+            className={classNames}
+        />
+    )
+}
