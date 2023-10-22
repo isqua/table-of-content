7216ebc6355703fd93d3ef46de7a7b114148395b
isqua <isqua@isqua.ru>
2023-10-22 21:04:19 +0300

    chore: setup testing for react components

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    // https://vitest.dev/config/
     test: {
         environment: 'jsdom',
+        setupFiles: './src/test/setup.ts',
+        css: {
+            modules: {
+                classNameStrategy: 'stable',
+            },
+        },
     },
 })
