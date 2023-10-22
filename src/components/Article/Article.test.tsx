14c12081a3dc2b6e4e39bd1b0311f4f11b37c9c8
isqua <isqua@isqua.ru>
2023-10-23 01:38:59 +0300

    feat: introduce the DocPage component for content rendering

---
+++ src/components/Article/Article.test.tsx
@@ -0,0 +1,20 @@
+import { act, render, screen } from '@testing-library/react'
+import { describe, expect, it } from 'vitest'
+
+import { Article } from './Article'
+
+describe('components/Article', () => {
+    it('should render an article', () => {
+        const page = {
+            id: 'foo',
+            parentId: '',
+            title: 'Foo Article'
+        }
+
+        act(() => {
+            render(<Article page={page} />)
+        })
+
+        expect(screen.getByRole('article')).toMatchSnapshot()
+    })
+})
