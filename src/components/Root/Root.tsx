80d0d0dd9f562777adca0c1c31c29039b61a5853
isqua <isqua@isqua.ru>
2023-10-22 22:51:14 +0300

    feat: setup browser routing

---
+++ src/components/Root/Root.tsx
@@ -0,0 +1,13 @@
+import { Layout } from '../Layout'
+
+export function Root() {
+    return (
+        <Layout>
+            <Layout.Sidebar>
+            </Layout.Sidebar>
+            <Layout.Main>
+                Hello world
+            </Layout.Main>
+        </Layout>
+    )
+}
