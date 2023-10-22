737d8f8cdead16b1b6aeb7273d1b31e879384257
isqua <isqua@users.noreply.github.com>
2023-10-22 23:58:45 +0300

    chore: setup vite-plugin-svgr for importing svg files to the react bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        svgr(),
+        react(),
+    ],
     // https://vitest.dev/config/
     test: {
         environment: 'jsdom',
