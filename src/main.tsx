261dd961f7e59a178e5ce1785de0e09ad7c4a620
isqua <isqua@isqua.ru>
2023-10-24 02:14:56 +0300

    refactor: move app-level components to the app directory

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import styles from './App.module.css'
-import App from './App.tsx'
+import styles from './app/App.module.css'
+import App from './app/App.tsx'
 
 const appRootElement = document.getElementById('root')
 
--- src/test/render.tsx
+++ src/test/render.tsx
@@ -1,7 +1,7 @@
 import type { PropsWithChildren, ReactElement } from 'react'
 import { render, renderHook, type RenderOptions } from '@testing-library/react'
 
-import { TestRouter } from '../components/Router'
+import { TestRouter } from '../app/Router'
 
 type AppProvidersProps = {
     url?: string
