64dd66de14e7dd12f8b25047a17def3c9b22357d
isqua <isqua@isqua.ru>
2023-10-25 00:27:15 +0300

    feat: add filter input to the menu

--- src/features/toc/ui/Menu/Context/hooks.ts
+++ src/features/toc/ui/Menu/Context/hooks.ts
@@ -1,12 +1,21 @@
-import { useContext } from 'react'
+import { useCallback, useContext, useEffect, useRef } from 'react'
 
 import { buildMenuSection } from '../../../core/buildMenuSection'
 import type { PageId, SectionHighlight } from '../../../types'
-import { LocationContext, TocContext } from './contexts'
+import { FilterContext, LocationContext, TocContext } from './contexts'
+
+const useFilterData = () => {
+    const { state: { data } } = useContext(FilterContext)
+
+    return data
+}
+
+const FILTER_DELAY_IN_MS = 1000
 
 export const useSectionItems = (parentId: PageId = '', level: number = 0, highlight: SectionHighlight) => {
     const { toc } = useContext(TocContext)
     const currentLocation = useContext(LocationContext)
+    const filter = useFilterData()
 
     const items = buildMenuSection(toc, {
         url: currentLocation.url,
@@ -14,6 +23,7 @@ export const useSectionItems = (parentId: PageId = '', level: number = 0, highli
         parentId,
         level,
         highlight,
+        filter,
     })
 
     return items
@@ -23,4 +33,37 @@ export const useIsLoading = () => {
     const { isLoading } = useContext(TocContext)
 
     return isLoading
+}
+
+export const useFilterInput = () => {
+    const { actions, state: { isLoading } } = useContext(FilterContext)
+    const { onChange, onFilterStart, onReset } = actions
+    const timeout = useRef<number>(0)
+
+    const onChangeHandler = useCallback((value: string) => {
+        const text = value.trim()
+
+        if (text === '') {
+            onReset()
+            return
+        }
+
+        if (!isLoading) {
+            onFilterStart()
+        }
+
+        if (timeout.current) {
+            clearTimeout(timeout.current)
+        }
+
+        timeout.current = window.setTimeout(() => {
+            onChange(text)
+        }, FILTER_DELAY_IN_MS)
+    }, [isLoading, onChange, onFilterStart, onReset])
+
+    useEffect(() => () => {
+        clearTimeout(timeout.current)
+    }, [])
+
+    return { onChange: onChangeHandler, isLoading }
 }
