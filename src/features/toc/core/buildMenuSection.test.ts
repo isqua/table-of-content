9386309be04d6757cead35a75be979f54fa7c7fc
isqua <isqua@isqua.ru>
2023-10-25 00:12:53 +0300

    feat: implement filtering in buildMenuSection

--- src/features/toc/core/buildMenuSection.test.ts
+++ src/features/toc/core/buildMenuSection.test.ts
@@ -2,10 +2,12 @@ import { describe, expect, it } from 'vitest'
 
 import type { TableOfContent } from '../types'
 import { buildMenuSection } from './buildMenuSection'
+import { filterTreeNodes } from './filterTreeNodes'
 import { getBreadCrumbs } from './getBreadCrumbs'
 
 import tocFlat from '../../../test/fixtures/toc/flat.json'
 import tocTwoLevels from '../../../test/fixtures/toc/two-levels.json'
+import tocThreeLevels from '../../../test/fixtures/toc/three-levels.json'
 
 describe('toc/buildMenuSection', () => {
     it('should build a menu and highlight current page', () => {
@@ -62,6 +64,31 @@ describe('toc/buildMenuSection', () => {
         const currentUrl = 'bar-features.html'
         const breadcrumbs = getBreadCrumbs(toc, currentUrl)
         const menu = buildMenuSection(toc, { url: currentUrl, parentId: 'bar', breadcrumbs })
+
+        expect(menu).toMatchSnapshot()
+    })
+
+    it('should filter pages', () => {
+        const toc: TableOfContent = tocFlat
+        const currentUrl = 'bar.html'
+        const breadcrumbs = getBreadCrumbs(toc, currentUrl)
+        const filter = filterTreeNodes(toc, 'ba')
+        const menu = buildMenuSection(toc, { url: currentUrl, breadcrumbs, filter })
+
+        expect(menu).toMatchSnapshot()
+    })
+
+    it('should expand all sections while filtering', () => {
+        const toc: TableOfContent = tocThreeLevels
+        const currentUrl = '/'
+        const breadcrumbs = getBreadCrumbs(toc, currentUrl)
+        const filter = filterTreeNodes(toc, 'features')
+
+        const menu = [
+            ...buildMenuSection(toc, { url: currentUrl, breadcrumbs, filter }),
+            ...buildMenuSection(toc, { parentId: 'bar', url: currentUrl, breadcrumbs, filter }),
+            ...buildMenuSection(toc, { parentId: 'bar_features', url: currentUrl, breadcrumbs, filter }),
+        ]
 
         expect(menu).toMatchSnapshot()
     })
