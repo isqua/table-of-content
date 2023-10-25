a259e9ff3de5817a60e189af440ca42ebb8523ee
isqua <isqua@users.noreply.github.com>
2023-10-25 21:34:05 +0300

    feat: show skeleton for search input while loading data

--- src/features/toc/ui/Menu/Filter/Filter.tsx
+++ src/features/toc/ui/Menu/Filter/Filter.tsx
@@ -1,18 +1,24 @@
+import clsx from 'clsx'
+
 import { Input } from '../../../../../components/Input'
-import { useFilterInput } from '../Context/hooks'
+import { Skeleton } from '../../../../../components/Skeleton'
+import { useFilterInput, useIsLoading } from '../Context/hooks'
 
 import styles from './Filter.module.css'
 
 export function Filter(): JSX.Element {
+    const isLoading = useIsLoading()
     const { onChange, isFiltering } = useFilterInput()
 
     return (
-        <div className={styles.filter}>
-            <Input
-                onChange={onChange}
-                isLoading={isFiltering}
-                placeholder='Filter menu'
-            />
+        <div className={clsx(styles.filter)}>
+            {isLoading ? (<Skeleton />) : (
+                <Input
+                    onChange={onChange}
+                    isLoading={isFiltering}
+                    placeholder='Filter menu'
+                />
+            )}
         </div>
     )
 }
