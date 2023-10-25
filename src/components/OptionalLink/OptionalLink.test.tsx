a7ebf90026d2cb3c799dd785d18432dc3561eddd
isqua <isqua@users.noreply.github.com>
2023-10-25 14:44:28 +0300

    feat: listen keyboard click on empty menu items

--- src/components/OptionalLink/OptionalLink.test.tsx
+++ src/components/OptionalLink/OptionalLink.test.tsx
@@ -1,62 +1,87 @@
 import { fireEvent, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { describe, expect, it, vi } from 'vitest'
 
 import { renderInApp } from '../../test'
 import { OptionalLink } from './OptionalLink'
 
 describe('components/OptionalLink', () => {
-    it('should render a link if to prop is passed', () => {
-        const text = 'Meow'
-        const href = '/meow'
-        const className = 'stylish'
+    describe('with url', () => {
+        it('should render a link if the ‘to’ prop is passed', () => {
+            const text = 'Meow'
+            const href = '/meow'
+            const className = 'stylish'
 
-        renderInApp(<OptionalLink className={className} to={href}>{text}</OptionalLink>)
+            renderInApp(<OptionalLink className={className} to={href}>{text}</OptionalLink>)
 
-        expect(screen.getByText(text).tagName).toEqual('A')
-        expect(screen.getByText(text)).toHaveAttribute('href')
-        expect(screen.getByText(text)).toHaveClass(className)
+            expect(screen.getByText(text).tagName).toEqual('A')
+            expect(screen.getByText(text)).toHaveAttribute('href')
+            expect(screen.getByText(text)).toHaveClass(className)
+        })
+
+        it('should call an onClick callback for a link', () => {
+            const text = 'Link'
+            const href = '/hello'
+            const onClick = vi.fn()
+
+            renderInApp(<OptionalLink to={href} onClick={onClick}>{text}</OptionalLink>)
+
+            fireEvent.click(screen.getByText(text))
+
+            expect(onClick).toBeCalledTimes(1)
+        })
     })
 
-    it('should render a text if to prop is empty string', () => {
-        const text = 'Stub'
-        const className = 'textish'
+    describe('with no url', () => {
+        it('should render a text if to prop is empty string', () => {
+            const text = 'Stub'
+            const className = 'textish'
 
-        renderInApp(<OptionalLink className={className} to="">{text}</OptionalLink>)
+            renderInApp(<OptionalLink className={className} to="">{text}</OptionalLink>)
 
-        expect(screen.getByText(text).tagName).not.toEqual('A')
-        expect(screen.getByText(text)).not.toHaveAttribute('href')
-        expect(screen.getByText(text)).toHaveClass(className)
-    })
+            expect(screen.getByText(text).tagName).not.toEqual('A')
+            expect(screen.getByText(text)).not.toHaveAttribute('href')
+            expect(screen.getByText(text)).toHaveClass(className)
+        })
 
-    it('should render a text if to prop is undefined', () => {
-        const text = 'Stub'
+        it('should render a text if to prop is undefined', () => {
+            const text = 'Stub'
 
-        renderInApp(<OptionalLink>{text}</OptionalLink>)
+            renderInApp(<OptionalLink>{text}</OptionalLink>)
 
-        expect(screen.getByText(text).tagName).not.toEqual('A')
-        expect(screen.getByText(text)).not.toHaveAttribute('href')
-    })
+            expect(screen.getByText(text).tagName).not.toEqual('A')
+            expect(screen.getByText(text)).not.toHaveAttribute('href')
+        })
 
-    it('should call an onClick callback for text', () => {
-        const text = 'Stub'
-        const onClick = vi.fn()
+        it('should call an onClick callback for text', () => {
+            const text = 'Stub'
+            const onClick = vi.fn()
 
-        renderInApp(<OptionalLink onClick={onClick}>{text}</OptionalLink>)
+            renderInApp(<OptionalLink onClick={onClick}>{text}</OptionalLink>)
 
-        fireEvent.click(screen.getByText(text))
+            fireEvent.click(screen.getByRole('button', { name: text }))
 
-        expect(onClick).toBeCalledTimes(1)
-    })
+            expect(onClick).toBeCalledTimes(1)
+        })
 
-    it('should call an onClick callback for a link', () => {
-        const text = 'Link'
-        const href = '/hello'
-        const onClick = vi.fn()
+        it('should call an onClick callback on enter press', async () => {
+            const text = 'Stub'
+            const onClick = vi.fn()
+            const user = userEvent.setup()
 
-        renderInApp(<OptionalLink to={href} onClick={onClick}>{text}</OptionalLink>)
+            renderInApp(<OptionalLink onClick={onClick}>{text}</OptionalLink>)
 
-        fireEvent.click(screen.getByText(text))
+            const link = screen.getByRole('button', { name: text })
 
-        expect(onClick).toBeCalledTimes(1)
+            await user.tab()
+
+            expect(link).toHaveFocus()
+
+            expect(onClick).not.toBeCalled()
+
+            await user.keyboard('[Enter]')
+
+            expect(onClick).toBeCalledTimes(1)
+        })
     })
 })
