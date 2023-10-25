daeb0e39468486dd2b12010e289ce6318c94ba4a
isqua <isqua@isqua.ru>
2023-10-25 14:44:28 +0300

    feat: listen keyboard click on empty menu items

--- src/components/OptionalLink/OptionalLink.tsx
+++ src/components/OptionalLink/OptionalLink.tsx
@@ -1,16 +1,47 @@
-import type { PropsWithChildren } from 'react'
+import type { KeyboardEvent, PropsWithChildren } from 'react'
 import { Link } from 'react-router-dom'
 
-export type OptionalLinkProps = PropsWithChildren<{
+type OptionalLinkProps = PropsWithChildren<{
     to?: string
     className?: string
     onClick?: () => void
 }>
+
+type ClickableLinkProps = PropsWithChildren<{
+    onClick: () => void
+    className?: string
+}>
+
+function captureKey(key: string, callback: () => void) {
+    return function (event: KeyboardEvent) {
+        if (event.key === key) {
+            callback()
+        }
+    }
+}
+
+function ClickableLink({ className, children, onClick }: ClickableLinkProps) {
+    return (
+        <span
+            tabIndex={0}
+            role="button"
+            className={className}
+            onClick={onClick}
+            onKeyUp={captureKey('Enter', onClick)}
+        >
+            {children}
+        </span>
+    )
+}
 
 export function OptionalLink({ to, className, children, onClick }: OptionalLinkProps) {
     if (to) {
         return (<Link to={to} className={className} onClick={onClick}>{children}</Link>)
     }
 
-    return (<span className={className} onClick={onClick}>{children}</span>)
+    if (onClick) {
+        return (<ClickableLink className={className} onClick={onClick}>{children}</ClickableLink>)
+    }
+
+    return (<span className={className}>{children}</span>)
 }
