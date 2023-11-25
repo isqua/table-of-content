b3080f913133ce09468630fbf90cbc0c5d9baf75
isqua <isqua@isqua.ru>
2023-11-25 23:15:55 +0300

    docs: explain how does the ui work

--- src/features/toc/types.ts
+++ src/features/toc/types.ts
@@ -1,6 +1,9 @@
 export type PageId = string
 export type PageURL = string
 
+/**
+ * Page descriptor from the API data
+ */
 export type PageDescriptor = {
     id: PageId
     title: string
@@ -9,12 +12,18 @@ export type PageDescriptor = {
     pages?: PageId[]
 }
 
+/**
+ * An index of pages by its id
+ */
 export type PagesIndex = Record<PageId, PageDescriptor | undefined>
 
 export type SectionHighlight = 'parent' | 'child' | undefined
 
 export type PageHighlight = 'active' | SectionHighlight
 
+/**
+ * The root object we take from the API
+ */
 export type TableOfContent = {
     entities: {
         pages: PagesIndex,
@@ -22,6 +31,9 @@ export type TableOfContent = {
     topLevelIds: PageId[]
 }
 
+/**
+ * The model of an item for the menu, containing all the data to render it
+ */
 export type MenuItem = {
     id: PageId
     parentId: PageId
