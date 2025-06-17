769af680f0eaee7f42fbf3860935c853a62982d9
isqua <isqua@users.noreply.github.com>
2025-06-17 21:39:44 +0200

    chore: upgrade eslint

---
+++ eslint.config.js
@@ -0,0 +1,62 @@
+// @ts-check
+import eslint from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import { globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  globalIgnores(['dist', 'eslint.config.js']),
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    plugins: {
+      js: eslint,
+      '@stylistic': stylistic
+    },
+    languageOptions: {
+      globals: {...globals.browser, ...globals.node}
+    }
+  },
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  reactPlugin.configs.flat.recommended,
+  reactPlugin.configs.flat['jsx-runtime'],
+  reactHooks.configs['recommended-latest'],
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-deprecated': ['off'],
+      '@typescript-eslint/no-empty-function': ['off'],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true
+        }
+      ],
+      '@stylistic/indent': ['error', 4],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/semi': ['error', 'never'],
+    }
+  }
+);
